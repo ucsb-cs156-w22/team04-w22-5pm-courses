5fd5061855487e62d2e8c7d8c4b216ea6b34591a
Aditya Sharma <adityasharma1709@gmail.com>
2022-03-09 13:27:24 -0800

    added level fixtures for testing

---
+++ frontend/src/fixtures/levelFixtures.js
@@ -0,0 +1,6 @@
+export const Alllevels = [["L","Undergrad-Lower"], 
+                            ["S","Undergrad-Upper Division"], 
+                            ["U","Undergrad-All"], 
+                            ["G","Graduate"]];
+
+export const singleLevel = ["U", "Undergrad-All"];
