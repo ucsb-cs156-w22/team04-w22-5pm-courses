41879bba47aaba18da1b37cb4bed68159dada803
rutvikjha <56047961+rutvikjha@users.noreply.github.com>
2022-03-09 14:22:06 -0800

    Update PersonalSchedulesCreatePage.test.js

--- frontend/src/tests/pages/PersonalSchedules/PersonalSchedulesCreatePage.test.js
+++ frontend/src/tests/pages/PersonalSchedules/PersonalSchedulesCreatePage.test.js
@@ -76,7 +76,7 @@ describe("PersonalSchedulesCreatePage tests", () => {
         
         const nameField = getByTestId("PersonalScheduleForm-name");
         const descriptionField = getByTestId("PersonalScheduleForm-description");
-        const quarterField = getByTestId("PersonalScheduleForm-quarter");
+        const quarterField = document.querySelector("#PersonalScheduleForm-quarter");
         const submitButton = getByTestId("PersonalScheduleForm-submit");
 
         fireEvent.change(nameField, { target: { value: 'SampName' } });
